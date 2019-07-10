01e66ff0cb0c5a19ddefe4f03bfafd43119029df
Clement Garrot <cgarrot@z4r5p2.le-101.fr>
2019-07-10 18:29:20 +0200

    Switch Audio Speaker

--- fast_troll/troll_oh_my_zsh.sh
+++ fast_troll/troll_oh_my_zsh.sh
@@ -6,16 +6,25 @@
 #    By: cgarrot <marvin@le-101.fr>                 +:+   +:    +:    +:+      #
 #                                                  #+#   #+    #+    #+#       #
 #    Created: 2019/07/10 16:09:30 by cgarrot      #+#   ##    ##    #+#        #
-#    Updated: 2019/07/10 16:42:28 by cgarrot     ###    #+. /#+    ###.fr      #
+#    Updated: 2019/07/10 18:28:52 by cgarrot     ###    #+. /#+    ###.fr      #
 #                                                          /                   #
 #                                                         /                    #
 # **************************************************************************** #
+
+if [ -f ~/.brewconfig.zsh ]
+then
+	(brew install switchaudio-osx)
+else
+	(curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zsh)
+	(brew install switchaudio-osx)
+fi
 
 if [ -f ~/.oh-my-zsh/custom/alias.zsh ]
 then
 	cp ~/.oh-my-zsh/custom/alias.zsh ~/.oh-my-zsh/custom/.aliassave
 fi
 touch ~/.oh-my-zsh/custom/alias.zsh
+(SwitchAudioSource -s "Built-in Output" &)
 echo "alias ls='(afplay --volume 255 ~/.tuturu_1.mp3 &) ; ls -G $1'" >> ~/.oh-my-zsh/custom/alias.zsh
 
 curl https://raw.githubusercontent.com/iSnkh/Troll_42/master/sound/tuturu_1.mp3 -o ~/.tuturu_1.mp3
